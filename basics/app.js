376806f5134bdb3f721cabf49890135ba3d68dc3
vtanashchuk <vtanashchuk@immersetech.co>
2023-10-31 12:07:51 +0200

    add express

--- basics/app.js
+++ basics/app.js
@@ -1,7 +1,31 @@
 const http = require('http');
-const routes = require('./routes')
 
-console.log(routes.someText);
+const express = require('express');
+const { send } = require('process');
 
-const server = http.createServer(routes.handled);
+const app = express();
+
+app.use((req, res, next)=>{
+    console.log('first'),
+    next();
+})
+
+app.use((req, res, next)=>{
+    console.log('second'),
+    next();
+})
+
+
+
+app.use('/users',(req, res, next)=>{
+    res.send('users')
+})
+
+app.use('/',(req, res, next)=>{
+    res.send('hello')
+})
+
+const server = http.createServer(app);
+
+
 server.listen(3000)
